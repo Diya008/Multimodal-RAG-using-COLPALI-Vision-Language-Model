63efaf4ad5c6a414e1dc32ead5d1c03a7eef66e3
Diya <122141883+Diya008@users.noreply.github.com>
2025-08-20 10:08:08 +0530

    Add files via upload

---
+++ load_once.py
@@ -0,0 +1,14 @@
+#This file is used to load the RAG model once and cache it for use in the main application.
+from byaldi import RAGMultiModalModel
+import streamlit as st
+
+@st.cache_resource
+def load_rag_model():
+    return RAGMultiModalModel.from_index(
+        index_path="information",       # folder name of your index
+        index_root=".byaldi",    # root folder where the index is stored
+        device="cpu",            # or "cuda" if GPU is available
+        verbose=1
+    )
+
+RAG = load_rag_model()
